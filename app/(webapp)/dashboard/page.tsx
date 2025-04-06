efee6740512d87530813ed366a7458f18399c0a4
FaizanahmedSy <saiyed.faizanahmed1@gmail.com>
2025-04-06 16:51:13 +0530

    switch repo add files to new repo

--- app/(webapp)/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
-        {/* <CreateEditMilestone /> */}
-      </div>
-    </div>
-  );
-}
---
+++ src/app/components/AppLanguageSwitcher/data.tsx
@@ -0,0 +1,20 @@
+import { LanguageProps } from '@/app/types/models/Apps';
+
+const languageData: LanguageProps[] = [
+  {
+    languageId: 'english',
+    locale: 'en',
+    name: 'English',
+  },
+  {
+    languageId: 'africans',
+    locale: 'af',
+    name: 'Afrikaans',
+  },
+  {
+    languageId: 'zulu',
+    locale: 'za',
+    name: 'IsiZulu',
+  },
+];
+export default languageData;
